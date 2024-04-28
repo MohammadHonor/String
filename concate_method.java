85a6e6f026d22ddc50a1c853da6a87c1d4e9d113
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-04-28 12:05:47 +0530

    concatnation

---
+++ concate_method.java
@@ -0,0 +1,10 @@
+public class concate_method {
+
+    public static void main(String[] args) {
+        String first="hi";
+        String sec="by";
+        String news=sec.concat(first);
+        System.out.println(news);
+    }
+    
+}
