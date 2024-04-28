cc030fd79ca1243f1e9082629e3aa77cb5429aeb
md.sharufali987@gmail.com <md.sharufali987@gmail.com>
2024-04-28 11:33:05 +0530

    join method

--- Example_01.java
+++ Example_01.java
@@ -2,9 +2,9 @@ public class Example_01{
     public static void main(String[] args) {
         String s="my name is khan";
         char [] ch = s.toCharArray();
-        System.out.println(ch);
+        System.out.print(ch);
         for( int i=0 ; i<ch.length; i++){
-            System.out.println(type(ch[i]));
+            System.out.print(ch[i]);
         }
     }
 }
---
+++ Join_method.java
@@ -0,0 +1,13 @@
+public class Join_method {
+
+    public static void main(String[] args) {
+        // join method 
+    String [] ch={"my","name","is","ali"};
+        String join_name=String.join("hi",ch);
+        String name=String.join("to ",ch);
+            
+    System.out.println(join_name+" \n"+name);
+    
+    }
+    
+}
